e724a90691669b331dd8c0228b290e300bb8e479
Unknown <goldennoob68@gmail.com>
2018-01-09 18:53:18 +0100

    menu contextuelle

--- MobileNetwork/Assets/Scripts/getInfos.cs
+++ MobileNetwork/Assets/Scripts/getInfos.cs
@@ -6,19 +6,25 @@ using UnityEngine.UI;
 public class getInfos : MonoBehaviour
 {
     private Image customImage;
+    private Text customText;
 
     private void Start()
     {
         customImage = FindObjectOfType<Image>();
+        customText = FindObjectOfType<Text>();
     }
 
     private void OnMouseOver()
     {
+        customText.text = "Position : (x : " + transform.position.x + ", y : " + transform.position.x + ")\n" +
+            "Fréquence : " + GetComponent<antennaData>().frequency + "\nPuissance : " + GetComponent<antennaData>().power;
         customImage.enabled = true;
+        customText.enabled = true;
     }
 
     private void OnMouseExit()
     {
         customImage.enabled = false;
+        customText.enabled = false;
     }
 }
